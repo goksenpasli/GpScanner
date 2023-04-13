f8b2dbc64558102c2a64baa8b17adcecbc6b7490
goksenpasli <gpasli@gmail.com>
2023-04-13 20:04:34 +0300

    GpScanner 2.0

--- GpScanner/TwainControl/XmlViewerControl.xaml.cs
+++ GpScanner/TwainControl/XmlViewerControl.xaml.cs
@@ -1,7 +1,4 @@
-﻿using System.IO;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
+﻿using System.Windows.Controls;
 
 namespace TwainControl
 {
@@ -10,29 +7,10 @@ namespace TwainControl
     /// </summary>
     public partial class XmlViewerControl : UserControl
     {
-        public static readonly DependencyProperty XmlContentProperty = DependencyProperty.Register(
-        "XmlContent", typeof(string), typeof(XmlViewerControl), new PropertyMetadata(default(string), OnXmlContentChanged));
-
         public XmlViewerControl()
         {
             InitializeComponent();
-        }
-
-        public string XmlContent {
-            get => (string)GetValue(XmlContentProperty);
-            set => SetValue(XmlContentProperty, value);
-        }
-
-        private static void OnXmlContentChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            if (d is XmlViewerControl xmlViewerControl && e.NewValue is string xmlfilepath && File.Exists(xmlfilepath))
-            {
-                FlowDocument document = xmlViewerControl.XmlRichTextBox?.Document;
-                document?.Blocks?.Clear();
-                Paragraph xmlParagraph = new();
-                xmlParagraph.Inlines.Add(new Run(File.ReadAllText(xmlfilepath)));
-                document?.Blocks?.Add(xmlParagraph);
-            }
+            DataContext = new XmlViewerControlModel();
         }
     }
 }
