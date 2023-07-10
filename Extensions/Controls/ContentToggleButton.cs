f6516e47a627a56e511ac31783459deb51554ca2
goksenpasli <gpasli@gmail.com>
2023-07-10 22:13:21 +0300

    Merge branch 'GpScanner-4.0' of https://github.com/goksenpasli/

--- Extensions/Controls/ContentToggleButton.cs
+++ Extensions/Controls/ContentToggleButton.cs
@@ -8,13 +8,12 @@ namespace Extensions;
 public class ContentToggleButton : ToggleButton
 {
     public static readonly DependencyProperty AlwaysOnTopProperty = DependencyProperty.RegisterAttached("AlwaysOnTop", typeof(bool), typeof(ContentToggleButton), new PropertyMetadata(true, OnTopChanged));
-    public static readonly DependencyProperty ContentHorizontalOffsetProperty =
-        DependencyProperty.Register("ContentHorizontalOffset", typeof(double), typeof(ContentToggleButton), new PropertyMetadata(0d));
-    public static readonly DependencyProperty ContentVerticalOffsetProperty =
-        DependencyProperty.Register("ContentVerticalOffset", typeof(double), typeof(ContentToggleButton), new PropertyMetadata(0d));
+    public static readonly DependencyProperty ContentHorizontalOffsetProperty = DependencyProperty.Register("ContentHorizontalOffset", typeof(double), typeof(ContentToggleButton), new PropertyMetadata(0d));
+    public static readonly DependencyProperty ContentVerticalOffsetProperty = DependencyProperty.Register("ContentVerticalOffset", typeof(double), typeof(ContentToggleButton), new PropertyMetadata(0d));
     public static readonly DependencyProperty CornerRadiusProperty = DependencyProperty.Register("CornerRadius", typeof(CornerRadius), typeof(ContentToggleButton), new PropertyMetadata(new CornerRadius(0d)));
     public static readonly DependencyProperty OverContentProperty = DependencyProperty.Register("OverContent", typeof(object), typeof(ContentToggleButton), new PropertyMetadata(null));
     public static readonly DependencyProperty PlacementModeProperty = DependencyProperty.Register("PlacementMode", typeof(PlacementMode), typeof(ContentToggleButton), new PropertyMetadata(PlacementMode.Bottom));
+    public static readonly DependencyProperty PopupAnimationProperty = DependencyProperty.Register("PopupAnimation", typeof(PopupAnimation), typeof(ContentToggleButton), new PropertyMetadata(PopupAnimation.Slide));
     public static readonly DependencyProperty StaysOpenProperty = DependencyProperty.Register("StaysOpen", typeof(bool), typeof(ContentToggleButton), new PropertyMetadata(false));
     public static readonly DependencyProperty TopMostProperty = DependencyProperty.Register("TopMost", typeof(bool), typeof(ContentToggleButton), new PropertyMetadata(true));
 
@@ -29,6 +28,8 @@ public class ContentToggleButton : ToggleButton
     public object OverContent { get => GetValue(OverContentProperty); set => SetValue(OverContentProperty, value); }
 
     public PlacementMode PlacementMode { get => (PlacementMode)GetValue(PlacementModeProperty); set => SetValue(PlacementModeProperty, value); }
+
+    public PopupAnimation PopupAnimation { get => (PopupAnimation)GetValue(PopupAnimationProperty); set => SetValue(PopupAnimationProperty, value); }
 
     public bool StaysOpen { get => (bool)GetValue(StaysOpenProperty); set => SetValue(StaysOpenProperty, value); }
 
--- Extensions/Controls/SplitButton.cs
+++ Extensions/Controls/SplitButton.cs
@@ -8,21 +8,15 @@ namespace Extensions;
 public class SplitButton : ButtonBase
 {
     public static readonly DependencyProperty AlwaysOnTopProperty = DependencyProperty.RegisterAttached("AlwaysOnTop", typeof(bool), typeof(SplitButton), new PropertyMetadata(true, OnTopChanged));
-    public static readonly DependencyProperty ContentHorizontalOffsetProperty =
-        DependencyProperty.Register("ContentHorizontalOffset", typeof(double), typeof(SplitButton), new PropertyMetadata(0d));
-    public static readonly DependencyProperty ContentVerticalOffsetProperty =
-        DependencyProperty.Register("ContentVerticalOffset", typeof(double), typeof(SplitButton), new PropertyMetadata(0d));
-    public static readonly DependencyProperty InternalContentProperty =
-        DependencyProperty.Register("InternalContent", typeof(object), typeof(SplitButton), new PropertyMetadata(null));
-    public static readonly DependencyProperty IsSplitPartOpenProperty =
-        DependencyProperty.Register("IsSplitPartOpen", typeof(bool), typeof(SplitButton), new PropertyMetadata(false));
+    public static readonly DependencyProperty ContentHorizontalOffsetProperty = DependencyProperty.Register("ContentHorizontalOffset", typeof(double), typeof(SplitButton), new PropertyMetadata(0d));
+    public static readonly DependencyProperty ContentVerticalOffsetProperty = DependencyProperty.Register("ContentVerticalOffset", typeof(double), typeof(SplitButton), new PropertyMetadata(0d));
+    public static readonly DependencyProperty InternalContentProperty = DependencyProperty.Register("InternalContent", typeof(object), typeof(SplitButton), new PropertyMetadata(null));
+    public static readonly DependencyProperty IsSplitPartOpenProperty = DependencyProperty.Register("IsSplitPartOpen", typeof(bool), typeof(SplitButton), new PropertyMetadata(false));
     public static readonly DependencyProperty PlacementModeProperty = DependencyProperty.Register("PlacementMode", typeof(PlacementMode), typeof(SplitButton), new PropertyMetadata(PlacementMode.Bottom));
-    public static readonly DependencyProperty SplitContentPartIsEnabledProperty =
-        DependencyProperty.Register("SplitContentPartIsEnabled", typeof(bool), typeof(SplitButton), new PropertyMetadata(true));
-    public static readonly DependencyProperty StayOpenProperty =
-        DependencyProperty.Register("StayOpen", typeof(bool), typeof(SplitButton), new PropertyMetadata(false));
-    public static readonly DependencyProperty TopMostProperty =
-        DependencyProperty.Register("TopMost", typeof(bool), typeof(SplitButton), new PropertyMetadata(true));
+    public static readonly DependencyProperty PopupAnimationProperty = DependencyProperty.Register("PopupAnimation", typeof(PopupAnimation), typeof(SplitButton), new PropertyMetadata(PopupAnimation.Slide));
+    public static readonly DependencyProperty SplitContentPartIsEnabledProperty = DependencyProperty.Register("SplitContentPartIsEnabled", typeof(bool), typeof(SplitButton), new PropertyMetadata(true));
+    public static readonly DependencyProperty StayOpenProperty = DependencyProperty.Register("StayOpen", typeof(bool), typeof(SplitButton), new PropertyMetadata(false));
+    public static readonly DependencyProperty TopMostProperty = DependencyProperty.Register("TopMost", typeof(bool), typeof(SplitButton), new PropertyMetadata(true));
 
     static SplitButton() { DefaultStyleKeyProperty.OverrideMetadata(typeof(SplitButton), new FrameworkPropertyMetadata(typeof(SplitButton))); }
 
@@ -35,6 +29,8 @@ public class SplitButton : ButtonBase
     public bool IsSplitPartOpen { get => (bool)GetValue(IsSplitPartOpenProperty); set => SetValue(IsSplitPartOpenProperty, value); }
 
     public PlacementMode PlacementMode { get => (PlacementMode)GetValue(PlacementModeProperty); set => SetValue(PlacementModeProperty, value); }
+
+    public PopupAnimation PopupAnimation { get => (PopupAnimation)GetValue(PopupAnimationProperty); set => SetValue(PopupAnimationProperty, value); }
 
     public bool SplitContentPartIsEnabled { get => (bool)GetValue(SplitContentPartIsEnabledProperty); set => SetValue(SplitContentPartIsEnabledProperty, value); }
 
