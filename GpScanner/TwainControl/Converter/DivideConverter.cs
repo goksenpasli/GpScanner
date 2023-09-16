ee60a3ab29d131d3a1d9abded8e74b866ed7280e
goksenpasli <gpasli@gmail.com>
2023-09-16 22:44:06 +0300

    Merge branch 'GpScanner-4.1' of https://github.com/goksenpasli/

---
+++ GpScanner/TwainControl/Converter/DivideConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace TwainControl.Converter;
+
+public sealed class DivideConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is double or int)
+        {
+            double inputValue = System.Convert.ToDouble(value);
+            double factor = System.Convert.ToDouble(parameter, CultureInfo.InvariantCulture);
+            return inputValue / factor;
+        }
+        return value;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) { throw new NotImplementedException(); }
+}
