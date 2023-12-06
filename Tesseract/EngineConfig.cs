ee41b71043209e75d77c0a2a2c95c27cede4a679
goksenpasli <gpasli@gmail.com>
2023-12-06 17:43:28 +0300

    Merge Win 7 branch 'GpScanner-5.0' of https://github.com/goksenpasli/

--- Tesseract/EngineConfig.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Tesseract
-{
-    public class EngineConfig
-    {
-        public string DataPath { get; set; }
-
-        public string Language { get; set; }
-    }
-}
--- Tesseract/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿#region Using directives
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-#endregion
-[assembly: ComVisible(false)]
-[assembly: InternalsVisibleTo("Tesseract.Net48Tests")]
-[assembly: InternalsVisibleTo("Tesseract.NetCore31Tests")]
