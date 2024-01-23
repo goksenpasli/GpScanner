61567d451aaee5b2a4adef29495b0ad5f0f0a5e2
goksenpasli <gpasli@gmail.com>
2024-01-23 20:38:14 +0300

    Merge branch 'GpScanner-5.0' of https://github.com/goksenpasli/GpScanner into GpScanner-5.0

---
+++ GpScanner/TwainControl/Converter/BitmapFrameToBitmapFrameConverter.cs
@@ -0,0 +1,15 @@
+﻿using Extensions;
+using System;
+using System.Drawing.Imaging;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace TwainControl.Converter;
+
+public sealed class BitmapFrameToBitmapFrameConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => value is BitmapFrame bitmapFrame ? BitmapFrame.Create(bitmapFrame.BitmapSourceToBitmap().ToBitmapImage(ImageFormat.Jpeg)) : null;
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+}
