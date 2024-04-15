8a18676138d1841291915e9ca86e279899a70b7b
goksenpasli <gpasli@gmail.com>
2024-04-15 20:25:23 +0300

    Merge branch 'GpScanner-5.0' of https://github.com/goksenpasli/GpScanner

--- Extensions/Controls/TranslateView.xaml.cs
+++ Extensions/Controls/TranslateView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace Extensions.Controls;
 
@@ -7,5 +8,18 @@ namespace Extensions.Controls;
 /// </summary>
 public partial class TranslateView : UserControl
 {
+    public static readonly DependencyProperty DestLangIsEnabledProperty = DependencyProperty.Register("DestLangIsEnabled", typeof(bool), typeof(TranslateView), new PropertyMetadata(true));
+    public static readonly DependencyProperty SourceLangIsEnabledProperty = DependencyProperty.Register("SourceLangIsEnabled", typeof(bool), typeof(TranslateView), new PropertyMetadata(true));
+
+    public bool DestLangIsEnabled {
+        get => (bool)GetValue(DestLangIsEnabledProperty);
+        set => SetValue(DestLangIsEnabledProperty, value);
+    }
+
+    public bool SourceLangIsEnabled {
+        get => (bool)GetValue(SourceLangIsEnabledProperty);
+        set => SetValue(SourceLangIsEnabledProperty, value);
+    }
+
     public TranslateView() { InitializeComponent(); }
 }
