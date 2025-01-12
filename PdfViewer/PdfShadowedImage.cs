4d29d020b73bf47c7c1d290aacb559906e9d03b2
goksenpasli <gpasli@gmail.com>
2025-01-12 15:14:18 +0300

    GpScanner

--- PdfViewer/PdfShadowedImage.cs
+++ PdfViewer/PdfShadowedImage.cs
@@ -7,14 +7,9 @@ public class PdfShadowedImage : ShadowedImage
 {
     private readonly Pen pen = new() { Thickness = 1.5 };
 
-    public PdfShadowedImage()
-    {
-        pen.Brush = ShadowColor;
-        pen.Freeze();
-    }
-
     protected override void OnRender(DrawingContext dc)
     {
+        pen.Brush = ShadowColor;
         dc.DrawRectangle(null, pen, new Rect(new Point(1, 1), new Size(ActualWidth, ActualHeight)));
         base.OnRender(dc);
     }
