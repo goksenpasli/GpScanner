febcff4a7fa7f5b4fd417bdddb702c358425dbbe
goksen <gpasli@gmail.com>
2025-06-15 19:42:02 +0300

    GpScanner

--- GpScanner/Properties/Settings.Designer.cs
+++ GpScanner/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace GpScanner.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.13.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.14.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -731,6 +731,18 @@ namespace GpScanner.Properties {
             }
             set {
                 this["ShowSuggestions"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool SimpleList {
+            get {
+                return ((bool)(this["SimpleList"]));
+            }
+            set {
+                this["SimpleList"] = value;
             }
         }
     }
